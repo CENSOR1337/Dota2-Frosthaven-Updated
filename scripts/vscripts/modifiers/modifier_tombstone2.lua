a2c98df94150c80d2e7ea9f13ce2339c6b516d0b
CENSOR_1337 <viixxivvimm@gmail.com>
2024-12-31 17:02:40 +0700

    fix: add revive conditions

--- scripts/vscripts/modifiers/modifier_tombstone2.lua
+++ scripts/vscripts/modifiers/modifier_tombstone2.lua
@@ -1,4 +1,4 @@
--- Thanks to: Sparking+
+-- Thanks to: Sparking+, ConRelly
 
 modifier_tombstone2 = class({})
 
@@ -19,17 +19,28 @@ end
 
 function modifier_tombstone2:OnIntervalThink()
     -- if not IsServer() then return end
-    local units = FindUnitsInRadius(self:GetParent():GetTeamNumber(), self:GetParent():GetOrigin(), nil, 300, DOTA_UNIT_TARGET_TEAM_FRIENDLY, 1, 48, FIND_ANY_ORDER, false)
-    if units[1] then
+    local parent = self:GetParent()
+    local parent_location = parent:GetOrigin()
+    local caster = self:GetCaster()
+    local revive_expected_value = 90
+
+    if caster:IsReincarnating() or caster:IsAlive() then -- or parent:HasModifier("modifier_arc_warden_tempest_double") then
+        self:GetParent():Destroy()
+    end
+
+    local units = FindUnitsInRadius(self:GetParent():GetTeamNumber(), parent_location, nil, 300, DOTA_UNIT_TARGET_TEAM_FRIENDLY, 1, DOTA_UNIT_TARGET_FLAG_NOT_ILLUSIONS, FIND_CLOSEST, false)
+    if units[1] and units[1]:IsRealHero() and units[1]:IsAlive() and not units[1]:IsStunned() and not units[1]:IsOutOfGame() and not units[1]:IsSilenced() and not units[1]:IsInvulnerable() then
         if not self.particle then
             self.particle = ParticleManager:CreateParticle("particles/econ/generic/generic_progress_meter/generic_progress_circle.vpcf", 0, self:GetParent())
             -- ParticleManager:SetParticleControl( self.particle,1,Vector(100,0,0) )
-            ParticleManager:SetParticleControl(self.particle, 0, self:GetParent():GetOrigin() + Vector(0, 0, 300))
+            ParticleManager:SetParticleControl(self.particle, 0, parent_location + Vector(0, 0, 300))
         end
         self:IncrementStackCount()
-        ParticleManager:SetParticleControl(self.particle, 1, Vector(100, self:GetStackCount() / 60, 0))
-        if self:GetStackCount() >= 60 then
-            self:GetCaster():RespawnHero(false, false)
+        ParticleManager:SetParticleControl(self.particle, 1, Vector(100, self:GetStackCount() / revive_expected_value, 0))
+        AddFOWViewer(DOTA_TEAM_BADGUYS, parent_location, 600, 6.0, false)
+        if self:GetStackCount() >= revive_expected_value then
+            caster:RespawnHero(false, false)
+            caster:SetAbsOrigin(parent_location)
         end
     else
         if self.particle then
